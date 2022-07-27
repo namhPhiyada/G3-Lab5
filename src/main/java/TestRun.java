26fad4a287f02aff0cd4643d057fd2a011e2c6e7
LENOVO <LENOVO@LAPTOP-7M7CPIOL>
2022-07-27 18:46:58 +0700

    first commit

---
+++ src/main/java/TestRun.java
@@ -0,0 +1,17 @@
+public class TestRun {
+    
+    public static void main (String [] args) {
+        
+    System.out.println ("========================== Staff ============================");    
+    
+    Staff ck = new Staff("Chanakarn Kingkaew","85/1 PIM Nonthaburi","PIM",1_000_000);
+    System.out.println(ck);
+    
+    System.out.println ("========================== Student ============================");
+    
+    Student cv = new Student("Kim Taeyeon","85/1 PIM Nonthaburi","CPE",2558,50000);
+    System.out.println(cv);
+    
+    }
+    
+}
